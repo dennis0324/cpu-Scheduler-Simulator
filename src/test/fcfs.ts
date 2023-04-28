925c2337b230da94861672110dbdd213270b282e
dennis ko <dennis0324@naver.com>
2023-04-28 21:14:11 +0900

    initialize all files because of unnessary upload files

---
+++ src/test/fcfs.ts
@@ -0,0 +1,8 @@
+import fcfs from "@type/fcfs"
+import {createProcess} from "@/schedulAlgorithm/CpuSchedular"
+debugger;
+const processArray = [...Array(5)].map((_, i) => createProcess(i, i, ~~(Math.random() * 10 ) + 1, i));
+
+const FCFS = new fcfs();
+// processArray.forEach(e => FCFS.push(e))
+console.log(FCFS.getResult(processArray))
