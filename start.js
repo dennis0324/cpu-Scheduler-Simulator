b86d16142e53735ad2c71f653420eabce4c2bcb7
dennis ko <dennis0324@naver.com>
2023-04-29 16:30:09 +0900

    add: file starter

---
+++ start.js
@@ -0,0 +1,13 @@
+const {execFile} = require('child_process')
+console.log(process.argv)
+
+let path = process.argv.find(e => e.indexOf("path") > -1)
+if(!path) throw new Error("Path not found")
+path = path.split(":")[1]
+
+const child = execFile('ts-node', ["-r", "tsconfig-paths/register",path], (error, stdout, stderr) => {
+    if (error) {
+       throw error;
+    }
+    console.log(stdout);
+});
