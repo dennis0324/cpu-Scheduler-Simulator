1eff7972f142f147e85c698c064b97de46f18562
dennis ko <dennis0324@naver.com>
2023-05-02 03:06:57 +0900

    remove: code due to adding new lib

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -18,18 +18,6 @@ function Home() {
       >
         <div className={'flex-col'}>
           <select></select>
-
-          {/* <div className={'grid grid-cols-3 h-10 border-2 rounded-xl'}>
-            <div className={"col-span-2 grid gird-cols-1 place-content-center"}>
-             <div>
-             {value}
-             </div>
-            </div>
-            <div className={"h-10 w-10 grid gird-cols-1 place-content-center"}>
-              <svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" className="css-8mmkcg"><path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path></svg>
-            </div>
-          </div> */}
-          
           <div className={'flex-col'}>
             <div onClick={() => setValue("fcfs")}>
               <span>First Come First Serve</span>
