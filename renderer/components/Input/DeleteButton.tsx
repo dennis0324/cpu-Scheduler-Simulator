01517d4a1f8ec5ca76433101071bbb2423777fa9
dennis ko <dennis0324@naver.com>
2023-05-04 01:38:06 +0900

    change: text2svg & dynamic size and color, hover event

--- renderer/components/Input/DeleteButton.tsx
+++ renderer/components/Input/DeleteButton.tsx
@@ -1,6 +1,18 @@
-function DeleteButton(onBUttonDelete){
+import { useState } from "react"
+import TrashIcon from "../svg/TrashIcon"
+
+function DeleteButton({index,onButtonDelete}){
+    const [color, setColor] = useState("#53646f")
+    const enter = () => setColor("rgb(251 113 133)")
+    const leave = () => setColor("#53646f")
     return(
-        <button onClick={onBUttonDelete}>delete</button>
+        <button 
+            onClick={() => onButtonDelete(index)}
+            onMouseEnter={() => enter()}
+            onMouseLeave={() => leave()}
+        >
+            <TrashIcon size={25} color={color}/>
+        </button>
     )
 }
 
