ac7c8b81817598903ab7ba01fde7a722e3acaa7b
dennis ko <dennis0324@naver.com>
2023-05-04 01:40:14 +0900

    add: Process Json Contorl Function

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,12 +1,48 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 
-import select from 'react-select'
-import Input from "../component/Input";
+import Input from "../components/Input";
+
+export type ControlProcess = {
+  processes:any[]
+  addProcessRow:Function,
+  removeProcessRow:Function,
+  changeValue:Function
+}
 
 function Home({selectAlgor}) {
   const [value, setValue] = useState('fcfs')
+  // const processesRef = useRef([]);
+  const [processes,setProcesses] = useState([]);
+
+  const addProcessRow = () => {
+    console.log(processes)
+    const currentProssess = [...processes]
+    currentProssess.push({
+      pid:-1,
+      brustTime:-1,
+      arrivalTime:-1,
+      priority:-1
+    })
+    setProcesses(currentProssess)
+  }
+
+  const removeProcessRow = (id) => {
+    console.log('delete' + id)
+    const currentProssess = [...processes]
+    currentProssess.splice(id,1)
+    console.log(currentProssess)
+
+    setProcesses(currentProssess)
+  }
+
+  const changeValue = (id,title,value) => {
+    const currentProssess = [...processes]
+    currentProssess[id][title] = value
+    setProcesses(currentProssess)
+    console.log(processes)
+  }
   // const 
   return (
     <React.Fragment>
@@ -14,21 +50,22 @@ function Home({selectAlgor}) {
         <title>Home - Nextron (with-typescript-tailwindcss)</title>
       </Head>
       <main
-        className={`flex min-h-screen  p-24 justify-center`}
+        className={`flex-col min-h-screen  p-24 justify-center`}
       >
+        {/* <ProcessContainer/> */}
         <div className={'flex-col'}>
-          <Input/>
+          <Input
+            processes={processes}
+            addProcessRow={addProcessRow}
+            removeProcessRow={removeProcessRow}
+            changeValue={changeValue}
+          />
         </div>
         
         <div className={'h-10 flex-col m-10'}>
           testing
         </div>
       </main>
-      <div className='mt-1 w-full flex-wrap flex justify-center'>
-        <Link href='/next'>
-          <a className='btn-blue'>Go to next page</a>
-        </Link>
-      </div>
     </React.Fragment>
   );
 }
