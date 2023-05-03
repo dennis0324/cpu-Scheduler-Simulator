c13d5ef88d1569a43b0864e8832e66f852419c93
dennis ko <dennis0324@naver.com>
2023-05-04 01:40:56 +0900

    add: table display

--- renderer/components/Input/ProcessContainer.tsx
+++ renderer/components/Input/ProcessContainer.tsx
@@ -1,13 +1,31 @@
-import { useEffect, useRef, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import style from "styled-components"
+import AddButton from "./AddButton";
 import Process from "./Process";
+import { OptionType } from "./SelectAlgor";
+import {ControlProcess} from '../../pages/home';
 
 
-const StyleCompo = style.div`
-  border: 1px solid #e5e7eb;
+const StyleCompo = style.table`
+  // border: 1px solid #e5e7eb;
+  border-collapse: seperate;
 `
-function ProcessConatiner(){
-  const processesRef = useRef([]);
+
+const StyledTHead = style.thead`
+background-color: #f8f8f8;
+`
+
+type InputProps = {
+  selectedAlgo: OptionType;
+  index:number;
+  setSelectedAlgo: Dispatch<SetStateAction<{}>>;
+  setArrivalTime: Dispatch<SetStateAction<number[]>>;
+  setBurstTime: Dispatch<SetStateAction<number[]>>;
+  setTimeQuantum: Dispatch<SetStateAction<number>>;
+  setPriorities: Dispatch<SetStateAction<number[]>>;
+};
+
+function ProcessConatiner(props:ControlProcess){
   const [needPriority, setNeedPriority] = useState(false);
 
   const handlePid = () => {
@@ -25,21 +43,42 @@ function ProcessConatiner(){
   
   }
   useEffect(() => {
-    processesRef.current.push({})
-  },[])
+    console.log("processchanged")
+    // props.processes.current?.push({})
+  },[props.processes])
+
+  const combine = (index) => {
+    return {
+      ...props,
+      index:index
+    }
+  }
+
+
   return(
-    <StyleCompo>
+    <StyleCompo className={''}>
+      <StyledTHead>
+        <tr className={'w-full'}>
+          <th className={'px-4 py-4'}>index</th>
+          <th className={'px-4 py-4'}>pid</th>
+          <th className={'px-4 py-4'}>brustTime</th>
+          <th className={'px-4 py-4'}>ArrivalTime</th>
+          <th className={'px-4 py-4'}>Priority</th>
+          <th className={'px-4 py-4'}>
+            <AddButton buttonPressed={props.addProcessRow}/>
+          </th>
+
+        </tr>        
+      </StyledTHead>
+      <tbody>
       {
-        processesRef.current.map((process, index) => <Process 
-        index = {index}
-        needPriority={needPriority}
-        handlePid={handlePid}
-        handleBrust={handleBrust}
-        handleArrival={handleArrival}
-        handlePriority={handlePriority}
-        key={index}
+        props.processes.map((process, index) => <Process 
+        {
+          ...combine(index)
+        }
         />)
       }
+      </tbody>
     </StyleCompo>
   )
 }
